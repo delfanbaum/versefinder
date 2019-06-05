a0e9d85294a5cd53a8bf8c51fba612d0d5a19cfe
delfanbaum <drelfanbaum@gmail.com>
2019-06-05 13:23:02 -0400

    got it to save text!

--- app/popup.js
+++ app/popup.js
@@ -1,35 +1,39 @@
+function getStoredText(){
+  chrome.storage.sync.get(['text'],function(verseText){
+    var textField = document.getElementById('verseTextField');
+    textField.innerHTML = verseText.text;
+  });
+};
+
+window.onload = getStoredText();
+
+
 var submit = document.getElementById("manualAddText");
 
 submit.onclick = function() {
-  var value = document.getElementById("manualText").value;
-  var textField = document.getElementById('verseTextField');
-  textField.innerHTML = value;
 
-  //clear text field
-  document.getElementById("manualText").value = "";
+  chrome.storage.sync.get('text', function(verseText){
+    var newText = '';
+    if (verseText === 'string'){
+      newText += String(verseText);
+    };
+
+    var inputText = document.getElementById("manualText").value;
+
+    if (inputText){
+      newText += String(inputText);
+    };
+
+
+    chrome.storage.sync.set({'text': newText});
+
+    // set the language to whatever
+    var textField = document.getElementById('verseTextField');
+    textField.innerHTML = newText;
+
+    //clear text field
+    document.getElementById("manualText").value = '';
+
+  });
 
 };
-
-//
-// chrome.storage.sync.get('text', function(verseText){
-//     var newText = "";
-//     if (verseText.text){
-//       newText += verseText.text
-//     }
-//
-//     var addText = document.getElementById('manualAdd').value;
-//
-//     if (addText){
-//       newText += addText;
-//     }
-//     chrome.storage.sync.set({'text': newText})
-//
-//     document.getElementById("manualAddText").value = '';
-//
-//     var pageText = document.getElementById('verseText');
-//     pageText.appendChild(newText);
-//
-//
-//
-// });
-//
