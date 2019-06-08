c67a186c8500108f0b837fbbeac4b1094979337d
delfanbaum <drelfanbaum@gmail.com>
2019-06-07 22:46:24 -0400

    work on context menu

--- app/eventPage.js
+++ app/eventPage.js
@@ -7,9 +7,30 @@ var contextMenuItem = {
 chrome.contextMenus.create(contextMenuItem);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
+   if (clickData.menuItemId == "spendMoney" && clickData.selectionText){
+     chrome.storage.sync.get('text', function(verseText){
+       var newText = '';
 
- if (clickData.menuItemID == "addText" && clickData.selectionText){
-     chrome.storage.sync.set({'text': String(clickData.selectionText)});
- };
+       console.log(newText + " 0")
 
-});
+       if (verseText){
+         newText += String(verseText.text);
+       };
+
+       console.log(newText + " 1")
+
+       var inputText = document.getElementById("manualText").value;
+
+       if (inputText){
+         if (inputText[0] != ' '){
+           newText += ' ' + inputText;
+         } else {
+           newText += inputText;
+         }
+       };
+
+       console.log(newText + " 2")
+
+       chrome.storage.sync.set({'text': newText });
+     });
+  }});
