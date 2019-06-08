1ae4d7c68fd8cf99e151ac3fe14307143fcca24c
delfanbaum <drelfanbaum@gmail.com>
2019-06-07 22:09:51 -0400

    adding correctly now!

--- app/popup.js
+++ app/popup.js
@@ -7,25 +7,34 @@ function getStoredText(){
 
 window.onload = getStoredText();
 
-
 var submit = document.getElementById("manualAddText");
 
 submit.onclick = function() {
 
   chrome.storage.sync.get('text', function(verseText){
     var newText = '';
-    if (typeof verseText === 'string'){
-      newText += String(verseText);
+
+    console.log(newText + " 0")
+
+    if (verseText){
+      newText += String(verseText.text);
     };
+
+    console.log(newText + " 1")
 
     var inputText = document.getElementById("manualText").value;
 
     if (inputText){
-      newText = newText + inputText;
+      if (inputText[0] != ' '){
+        newText += ' ' + inputText;
+      } else {
+        newText += inputText;
+      }
     };
 
+    console.log(newText + " 2")
 
-    chrome.storage.sync.set({'text': newText});
+    chrome.storage.sync.set({'text': newText });
 
     // set the language to whatever
     var textField = document.getElementById('verseTextField');
@@ -38,4 +47,11 @@ submit.onclick = function() {
 
   });
 
+};
+
+var reset = document.getElementById("resetStorage");
+reset.onclick = function() {
+  chrome.storage.sync.set({'text': ' ' });
+  var textField = document.getElementById('verseTextField');
+  textField.innerHTML = ' ';
 };
