b56bb8621bb6f68e5bcd666cf978354f8e228243
delfanbaum <drelfanbaum@gmail.com>
2019-06-13 10:46:12 -0400

    context menus working

--- app/eventPage.js
+++ app/eventPage.js
@@ -14,35 +14,36 @@ chrome.contextMenus.create(contextMenuItemAddText);
 chrome.contextMenus.create(contextMenuItemAddTextAndGo);
 
 chrome.contextMenus.onClicked.addListener(function(clickData){
-   if (clickData.menuItemId == "addTextFromSelection" && clickData.selectionText){
-     chrome.storage.sync.get('text', function(verseText){
-       // get text and url
-       var selectionText = clickData.selectionText;
-       var selectionSourceUrl = clickData.pageUrl;
-       // other stuff
-       var newText = '';
-       // console.log(newText + " 0")
+  if (clickData.selectionText){
+    var selectionText = clickData.selectionText;
+    var selectionSourceUrl = clickData.pageUrl;
 
-       if (verseText){
-         newText += String(verseText.text);
-       };
-       // console.log(newText + " 1")
-
-       var newTextGroup = `
-       <div class="verseTextGroup" id="source-${selectionSourceUrl}">
-         <p class="verseText">${selectionText}</p>
-         <p class="textSource">Source: <a href="${selectionSourceUrl}"  target=”_blank”>${selectionSourceUrl}</a></p>
-       </div>
-       `
-       newText += newTextGroup;
-       // console.log(newText + " 2")
-       chrome.storage.sync.set({'text': newText });
-     });
-  } else if (clickData.menuItemId == "addTextAndGo" && clickData.selectionText) {
-    window.open('options.html');
-  }
+    if (clickData.menuItemId == 'addTextFromSelection') {
+      updateText(selectionText, selectionSourceUrl);
+    } else if (clickData.menuItemId == 'addTextAndGo'){
+      updateText(selectionText, selectionSourceUrl);
+      window.open('options.html');
+    };
+  };
 });
 
-function updateText(){
 
-}
+function updateText(selectionText, selectionSourceUrl){
+  chrome.storage.sync.get('text', function(verseText){
+    var newText = '';
+
+    if (verseText){
+      newText += String(verseText.text);
+    };
+
+    var newTextGroup = `
+    <div class="verseTextGroup" id="source-${selectionSourceUrl}">
+      <p class="verseText">${selectionText}</p>
+      <p class="textSource">Source: <a href="${selectionSourceUrl}"  target=”_blank”>${selectionSourceUrl}</a></p>
+    </div>
+    `
+    newText += newTextGroup;
+    // console.log(newText + " 2")
+    chrome.storage.sync.set({'text': newText });
+  })
+};
